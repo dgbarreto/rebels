6d895387ef39a9c62d9caf621a9ca1f892640646
BSBR\X241000 <danilo.barreto@prservicos.com.br>
2021-05-22 08:27:17 -0300

    Initial Commit

---
+++ RebelLocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RebelLocator
+//
+//  Created by Danilo Barreto on 22/05/21.
+//  Copyright © 2021 Danilo Barreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

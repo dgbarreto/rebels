c7a480edca3f7f7c08b974452f70950b9135a77d
BSBR\X241000 <danilo.barreto@prservicos.com.br>
2021-05-22 09:36:35 -0300

    Initial version of the project

--- RebelLocator/ViewController.swift
+++ RebelLocator/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    @IBOutlet weak var rebelTable : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "rebelCellId", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let seenAction = UIContextualAction(style: .normal, title: "Seen"){ (action, view, completion) in
+            completion(true)
+        }
+        
+        seenAction.backgroundColor = .red
+        let actionConfig = UISwipeActionsConfiguration(actions: [seenAction])
+        return actionConfig
+    }
 }
 
